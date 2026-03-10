16bc1849ca78667d4674486c8eb6ca577bbc2a22
gumba <gumba6740@gmail.com>
2026-03-10 18:56:22 +0900

    feat: 1페이지 스크래핑 코드 작성
    - aerich 설치
    - 명언, 저자로 분리 성공
    - 명언 모델 수정

--- app/scrapers/wise_word_scraper.py
+++ app/scrapers/wise_word_scraper.py
@@ -31,4 +31,20 @@ headers = {
 res = requests.get(url=url, headers=headers, cookies=cookies)
 res.raise_for_status()
 
-print(f"상태 코드: {res.status_code}")
+# 첫번째 table 태그의 tr 태그 내용을 전부 가져옴(table2, 리스트 상태)
+# 필요한 부분만 슬라이싱(table3)
+html = bs(res.content, 'lxml')
+table1 = html.find('table')
+table2 = table1.find_all('tr')
+table3 = table2[2::2]
+
+# 명언과 저자를 분리. 만일 저자가 없을 시 무시하도록 처리
+for td in table3:
+    td_list = list(td.stripped_strings)
+    if len(td_list) < 2:
+        continue
+    wise_word = td_list[0]
+    author = td_list[1]
+    print(wise_word)
+    print(author)
+    print("-"*20)
