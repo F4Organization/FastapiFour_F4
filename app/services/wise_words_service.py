c01692e54f52b7a50958a22e8559ae48785e2ae3
gumba <gumba6740@gmail.com>
2026-03-11 15:15:05 +0900

    feat: 북마크 등록 서비스

--- app/services/wise_words_service.py
+++ app/services/wise_words_service.py
@@ -1,5 +1,7 @@
 import random
 from app.models.wise_word_model import WiseWord, BookmarkedWiseWord
+from fastapi import HTTPException
+
 
 async def get_random_wise_word() -> WiseWord:
     wise_word_id = random.randint(31, 15206)
@@ -7,4 +9,13 @@ async def get_random_wise_word() -> WiseWord:
     if not wise_word:
         wise_word = await WiseWord.all().order_by("?").first()
     return wise_word
+
+
+async def add_bookmark(user_id: int, wise_word_id: int) -> dict:
+    wise_word = await BookmarkedWiseWord.filter(user_id=user_id, wise_word_id=wise_word_id).first()
+    if wise_word:
+        raise HTTPException(status_code=400, detail="Wise word already bookmarked")
+    else:
+        await BookmarkedWiseWord.create(user_id=user_id, wise_word_id=wise_word_id)
+        return {"message": "Bookmarked successfully"}
 
